9ddd5c8ee891b5348b0a359025416cc57124e6d4
Vasyl Khomko <homko.wasyl@gmail.com>
2018-09-26 10:38:30 +0300

    Add files via upload

---
+++ replace-chrome.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt remove google-chrome-stable
+cd /home/seluser
+wget https://github.com/vasyl-khomko/google-chrome-old-version/raw/master/chrome64_58.0.3029.96.deb -O chrome64_58.0.3029.96.deb
+sudo apt install ./chrome64_58.0.3029.96.deb
+cd /opt/selenium
+wget https://github.com/vasyl-khomko/google-chrome-old-version/raw/master/chromedriver -O chromedriver
+sudo chmod 777 chromedriver
+sudo ln -sf /opt/selenium/chromedriver /usr/bin/chromedriver
