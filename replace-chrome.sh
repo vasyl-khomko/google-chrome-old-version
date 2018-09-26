e58ebd42cd67c077cbd32b936d131bf5bf5b8542
Vasyl Khomko <homko.wasyl@gmail.com>
2018-09-26 10:43:19 +0300

    Update replace-chrome.sh

--- replace-chrome.sh
+++ replace-chrome.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt remove google-chrome-stable
+sudo apt --yes remove google-chrome-stable
 cd /home/seluser
 wget https://github.com/vasyl-khomko/google-chrome-old-version/raw/master/chrome64_58.0.3029.96.deb -O chrome64_58.0.3029.96.deb
 sudo apt --yes install ./chrome64_58.0.3029.96.deb
